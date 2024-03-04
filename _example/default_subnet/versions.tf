3d0cff3fbf0674c5306317cc72c3ffc40a35b540
krishana <smilekrishana@gmail.com>
2024-03-04 16:53:11 +0530

    inital commit

--- _example/default_subnet/versions.tf
+++ _example/default_subnet/versions.tf
@@ -1,13 +1,13 @@
 # Terraform version
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.7.4"
 }
 
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=2.90.0"
+      version = ">=3.87.0"
     }
   }
 }
